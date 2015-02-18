550ab6acbcd18d47f05ef93afaf33e9589ec94ef
aankit <aankit.patel@gmail.com>
2015-02-18 13:48:54 -0500

    first commit

---
+++ README.md
@@ -0,0 +1,3 @@
+#TraceJSON
+
+TraceJSON allows you to get the ipinfo of the nodes in your traceroute in JSON format.
---
+++ tracejson.py
@@ -0,0 +1,33 @@
+import sys, os
+import subprocess
+
+filename = sys.argv[1]
+output = 'output_'+filename
+
+f = open(filename, 'r')
+outputFile = open(output, 'w')
+outputFile.write('traceroute:[')
+outputFile.close()
+
+lastID = 0
+for line in f:
+	line = line.strip()
+	f = line.find('(')
+	l = line.find(')')
+	ip = line[f+1:l]
+	ipinfo = subprocess.Popen(['curl', 'ipinfo.io/%s' %(ip)], stdout=subprocess.PIPE)
+	(out, err) = ipinfo.communicate()
+	with open(output, "a") as outputFile:
+		if out:
+			outputFile.write(out)
+			outputFile.write(',')
+		else:
+			pass
+
+with open(output, 'rb+') as outputFile:
+	outputFile.seek(-1, os.SEEK_END)
+	outputFile.truncate()
+outputFile.close()
+outputFile = open(output, 'a')
+outputFile.write(']')
+outputFile.close()
